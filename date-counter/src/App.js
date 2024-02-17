65e692cdc824a43fa0965ed730a80f0d388bbf19
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-02-17 19:43:23 +0400

    date counter v2

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -1,32 +1,44 @@
-import { useState } from 'react';
-
+import { useState } from 'react'
+import './App.css'
 function App() {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1)
+  const [count, setCount] = useState(0)
 
-  const currentDate = new Date();
+  const currentDate = new Date()
 
   const dateFormat = new Intl.DateTimeFormat('en-US', {
     year: 'numeric',
     month: 'short',
     day: '2-digit',
     weekday: 'long',
-  }).format(count * 1000 * 60 * 60 * 24 + currentDate.getTime());
+  }).format(count * 1000 * 60 * 60 * 24 + currentDate.getTime())
 
-  function handleStepDecr() {
-    if (step > 1) setStep((s) => s - 1);
+  function handleReset() {
+    setStep(1)
+    setCount(0)
   }
 
   return (
     <div className='App'>
       <div className='step'>
-        <button onClick={handleStepDecr}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        {step}
+        <input
+          type='range'
+          value={step}
+          min={1}
+          max={10}
+          onChange={(e) => setStep(+e.target.value)}
+        />
       </div>
       <div className='count'>
         <button onClick={() => setCount((s) => s - step)}>-</button>
-        <span>Count: {count}</span>
+        <span>
+          <input
+            type='text'
+            value={count}
+            onChange={(e) => setCount(+e.target.value)}
+          />
+        </span>
         <button onClick={() => setCount((s) => s + step)}>+</button>
       </div>
       <div className='date'>
@@ -39,8 +51,9 @@ function App() {
         </span>
         <span>{dateFormat}</span>
       </div>
+      <button onClick={handleReset}>Reset</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
